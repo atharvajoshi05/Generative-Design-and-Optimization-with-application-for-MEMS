6d8c33c36146c2b8ce7593dc40c4152f87960d54
atharvajoshi05 <56198727+atharvajoshi05@users.noreply.github.com>
2022-07-12 20:42:55 +0530

    Add files via upload

---
+++ optimise.py
@@ -0,0 +1,18 @@
+﻿#!/usr/bin/env python
+
+# Import required modules:
+from sys import argv
+import topy
+
+
+def optimise(fname):
+    # Set up ToPy:
+    t = topy.Topology()
+    t.load_tpd_file(fname)
+    t.set_top_params()
+    topy.optimise(t)
+
+
+if __name__ == '__main__':
+    optimise(argv[1])
+
